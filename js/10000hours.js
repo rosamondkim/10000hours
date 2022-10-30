2a3b71c2aeb13cabedb9ca3556b148f784d4cf7a
rosamondkim <itzmems@naver.com>
2022-10-30 15:11:08 +0900

    모달창 css 변경완료

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,12 +1,12 @@
-// js file
 const excBtn = document.querySelector(".btn-exc");
 
 const input = document.querySelector(".cont-inp");
-const wannabe = document.querySelector(".txt-wannabe");
+//폼
+const wannabe = input.querySelector(".txt-wannabe");
 //첫번재인풋 wannabe
-const inpWannabe = document.querySelector(".input-wannabe");
+const inpWannabe = input.querySelector(".input-wannabe");
 // wannabe 내의 인풋
-const inpTime = document.querySelector(".input-time");
+const inpTime = input.querySelector(".input-time");
 // time 인풋
 
 const result = document.getElementById("result");
@@ -16,36 +16,30 @@ const goBtn = document.querySelector(".btn-go");
 const modal = document.querySelector(".cont-modal");
 
 
+
 function calc10000hours(){
     const userTime = parseInt(inpTime.value);
     timeResult.innerText= Math.ceil(10000/userTime);
 }
 
 function showResult(){
-    // input 의 유효성검사
+    // 유효성검사
     if(inpTime.value<=0||inpTime.value>24||isNaN(inpTime.value-1)||inpWannabe.value===" "){
         alert('제대로 입력하세요!!');
         return
     }
     result.classList.remove("hidden");
     wannabeResult.innerText = inpWannabe.value;
-    calc10000hours(inpTime);
+    calc10000hours();
     inpWannabe.value="";
     inpTime.value="";
 }
 excBtn.addEventListener("click",showResult);
 
 
-// 문제점 3) css -> 모달창 전체화면으로 변경하기
-
 goBtn.addEventListener("click",()=>{
     modal.classList.remove("hidden");
+    document.body.classList.add("stop-scroll");
 });
 
 
-
-// 1)    if (inpTime.value === NaN || inpWannabe.value ===""){
-//     return false;
-//     // 궁금함 1) return 이 뭔지?
-//     // 문제점 1) 하나만 입력했을때에 결과 나와버림
-// }
--- readme.md
+++ readme.md
@@ -3,7 +3,9 @@
 figma 주소:
 https://www.figma.com/file/YIoHn24LhFrCBEedo96rx6/1%EB%A7%8C-%EC%8B%9C%EA%B0%84%EC%9D%98-%EB%B2%95%EC%B9%99?node-id=0%3A1
 
-page 주소
-https://rosamondkim.github.io/10000hours/
 
-html/css 까지 진행했습니다
+page 주소
+https://paullab.co.kr/10000hours.html
+
+GitHub page 주소
+https://rosamondkim.github.io/10000hours/
