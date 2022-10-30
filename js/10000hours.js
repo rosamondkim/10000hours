7d4badd96e3bc8b79a792db2a0678395a54bc2af
rosamondkim <itzmems@naver.com>
2022-10-30 18:27:01 +0900

    자바스크립트 코드 리팩토링

--- js/10000hours.js
+++ js/10000hours.js
@@ -3,7 +3,7 @@ const excBtn = document.querySelector(".btn-exc");
 const input = document.querySelector(".cont-inp");
 //폼
 const wannabe = input.querySelector(".txt-wannabe");
-//첫번재인풋 wannabe
+//첫번째 인풋 wannabe
 const inpWannabe = input.querySelector(".input-wannabe");
 // wannabe 내의 인풋
 const inpTime = input.querySelector(".input-time");
@@ -15,8 +15,6 @@ const timeResult = document.querySelector(".result-time");
 const goBtn = document.querySelector(".btn-go");
 const modal = document.querySelector(".cont-modal");
 
-
-
 function calc10000hours(){
     const userTime = parseInt(inpTime.value);
     timeResult.innerText= Math.ceil(10000/userTime);
@@ -24,9 +22,13 @@ function calc10000hours(){
 
 function showResult(){
     // 유효성검사
-    if(inpTime.value<=0||inpTime.value>24||isNaN(inpTime.value-1)||inpWannabe.value===" "){
-        alert('제대로 입력하세요!!');
-        return
+    let timeValue = inpTime.value;
+    let wannabeValue = inpWannabe.value;
+    // isNaN -1
+    if (timeValue > 24 || timeValue <= 0 || isNaN(timeValue) || !(wannabeValue) || wannabeValue==" "
+    ) {
+        alert('올바르게 입력해주세요!')
+        return;
     }
     result.classList.remove("hidden");
     wannabeResult.innerText = inpWannabe.value;
@@ -43,3 +45,7 @@ goBtn.addEventListener("click",()=>{
 });
 
 
+// 추가기능: 공유하기 버튼 눌렀을때 링크 복사되도록
+// 추가기능: 모달창 바깥 누르면, 모달창 닫히고 새로고침되게
+
+// 문제 1 : 제대로 입력해도, input창에 제대로 입력하라는 required
